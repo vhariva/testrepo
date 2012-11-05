98c304a14b010780ecc0f62aa3b644b6a4be3c90
Hariharan A V <vasanthakumar.hariharan@pearson.com>
2012-11-05 12:27:53 -0600

    Added

---
+++ Testnav.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import java.util.List;
+
+import models.Item;
+import models.ItemFlavor;
+import models.Testmap;
+import play.mvc.*;
+
+public class Testnav extends Application {
+
+	@Secure
+    public static void index(Long tmId, int itemIndex) {
+    	
+        Testmap testmap = Testmap.findById(tmId);
+        notFoundIfNull(testmap, "The Testmap with (Id="+tmId+") was not found.");
+
+        int itemCount = testmap.testmapItems.size();
+        if (itemCount == 0) {
+        	notFound("Invalid Testmap.  No Items found.");
+        } else if (itemIndex >= itemCount) {
+        	notFound("Invalid Item Index.  No Items found.");
+        }
+        
+        String testmapArrayString = "";
+		for (int i=0; i < itemCount; i++) {
+			if (i == 0) {
+				testmapArrayString += testmap.testmapItems.get(i).item.id.toString();
+			} else {
+				testmapArrayString += "," + testmap.testmapItems.get(i).item.id.toString();
+			}
+		}
+		testmapArrayString = "[" + testmapArrayString + "]"; 
+		
+		Long itemId = testmap.testmapItems.get(itemIndex).item.id;
+		Item item = Item.findById(itemId);
+		notFoundIfNull(item, "Unable to find requested Item with (Id="+itemId+").");
+		
+		ItemFlavor itemFlavor = item.getItemFlavorByFlavor(1);
+        render(testmap, testmapArrayString, itemIndex, item, itemFlavor);
+    }
+
+}
